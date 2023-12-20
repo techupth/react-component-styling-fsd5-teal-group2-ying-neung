7ed98e6a7b5a7a41feef967b1da79d6ca3473e6b
suphattraying <suphattra.spk@gmail.com>
2023-12-20 12:37:35 +0700

    finish

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,108 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+function Alert(props) {
+  if (props.text.includes("error")) {
+    return (
+        <>
+      <div 
+      css={css`
+      display:flex;
+      align-items:center;
+      width: 650px;
+      height: 50px;     
+      background-color:#F9C8C8;
+      border-radius: 10px;
+      padding:15px;
+      gap:15px;
+      `}
+      >
+        <img css={css`;width:24px; height: 70px;`}src="./src/image/frown.svg" alt="frown" />
+        <p 
+        css={css`color: #444;
+font-family: Kanit;
+font-size: 20px;
+font-weight: 500;
+`}>This is {props.text} alert box</p>
+      </div>
+      </>
+    );
+  }else if (props.text.includes("warning")) {
+    return (
+        <>
+      <div 
+      css={css`
+      display:flex;
+      align-items:center;
+      width: 650px;
+      height: 50px;     
+      background-color:#F9D9C8;
+      border-radius: 10px;
+      padding:15px;
+      gap:15px;
+      `}
+      >
+        <img css={css`;width:24px; height: 70px;`}src="./src/image/alert-triangle.svg" alt="alert-triangle" />
+        <p 
+        css={css`color: #444;
+font-family: Kanit;
+font-size: 20px;
+font-weight: 500;
+`}>This is {props.text} alert box</p>
+      </div>
+      </>
+    );
+  }else if (props.text.includes("info")) {
+    return (
+        <>
+      <div 
+      css={css`
+      display:flex;
+      align-items:center;
+      width: 650px;
+      height: 50px;     
+      background-color:#F9EBC8;
+      border-radius: 10px;
+      padding:15px;
+      gap:15px;
+      `}
+      >
+        <img css={css`;width:24px; height: 70px;`}src="./src/image/alert-circle.svg" alt="alert-circle" />
+        <p 
+        css={css`color: #444;
+font-family: Kanit;
+font-size: 20px;
+font-weight: 500;
+`}>This is {props.text} alert box</p>
+      </div>
+      </>
+    );
+  }else if (props.text.includes("success")) {
+    return (
+        <>
+      <div 
+      css={css`
+      display:flex;
+      align-items:center;
+      width: 650px;
+      height: 50px;     
+      background-color:#CEF7CD;
+      border-radius: 10px;
+      padding:15px;
+      gap:15px;
+      `}
+      >
+        <img css={css`;width:24px; height: 70px;`}src="./src/image/check-circle.svg" alt="check-circle" />
+        <p 
+        css={css`color: #444;
+font-family: Kanit;
+font-size: 20px;
+font-weight: 500;
+`}>This is {props.text} alert box</p>
+      </div>
+      </>
+    );
+  }
+}
 
-function Alert() {}
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,84 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+function Button(props) {
+  if (props.type === "Primary") {
+    return (
+      <button
+       css={css`
+		padding: 20px;
+		background-color:#074EE8;
+		fontSize: 24px;
+		border: none;
+		borderRadius: 5px;
+		fontWeight: bold;
+		textAlign: center;
+		color:white;
+    width:200px;
+	 `}
+      >
+        Button
+      </button>
+    );
+  } else if (props.type === 'Secondary') {
+return (
+  <button
+   css={css`
+padding: 20px;
+background-color:#07A4E8;
+fontSize: 24px;
+border: none;
+borderRadius: 5px;
+fontWeight: bold;
+textAlign: center;
+color:white;
+width:200px;
+`}
+  >
+    Button
+  </button>
+);
+  
+  }  else if (props.type === 'Error') {
+    return (
+      <button
+       css={css`
+    padding: 20px;
+    background-color:#DE5753;
+    fontSize: 24px;
+    border: none;
+    borderRadius: 5px;
+    fontWeight: bold;
+    textAlign: center;
+    color:white;
+    width:200px;
+    `}
+      >
+        Button
+      </button>
+    );
+      
+      }else if (props.type === 'Success') {
+        return (
+          <button
+           css={css`
+        padding: 20px;
+        background-color:#26B795;
+        fontSize: 24px;
+        border: none;
+        borderRadius: 5px;
+        fontWeight: bold;
+        textAlign: center;
+        color:white;
+        width:200px;
+        `}
+          >
+            Button
+          </button>
+        );
+          
+          }
 
-function Button() {}
+}
+
+  export default Button
